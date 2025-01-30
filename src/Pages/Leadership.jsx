5470470ad01dcc7779d5d137a69e168e1e444db8
Rosa Lucena <rluc.sal@gmail.com>
2025-01-30 09:51:15 +0000

    fixexd pictures leadership

--- src/Pages/Leadership.jsx
+++ src/Pages/Leadership.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "../App.css";
+import mariana from "../img/mariana.jpeg";
+import caitlin from "../img/caitlin.jpeg";
+import dantiu from "../img/dantiu.jpeg";
+import nadia from "../img/nadia.jpeg";
+import diana from "../img/diana.jpeg";
 
 const Leadership = () => {
   const leaders = [
-    { name: "Marianna Verlage", title: "President", image: "src/img/mariana.jpeg" },
-    { name: "Caitlin Lewis", title: "Vice President", image: "src/img/caitlin.jpeg" },
-    { name: "DanThuy Chu", title: "Treasurer", image: "src/img/dan tiu.jpeg" },
-    { name: "Nadia Eldeib", title: "Secretary", image: "src/img/nadia.jpeg" },
-    { name: "Diana Powell", title: "Membership Chair", image: "src/img/diana.jpeg" },
+    { name: "Marianna Verlage", title: "President", image: mariana },
+    { name: "Caitlin Lewis", title: "Vice President", image: caitlin },
+    { name: "DanThuy Chu", title: "Treasurer", image: dantiu },
+    { name: "Nadia Eldeib", title: "Secretary", image: nadia},
+    { name: "Diana Powell", title: "Membership Chair", image: diana },
   ];
 
   return (
@@ -20,15 +25,15 @@ const Leadership = () => {
         <h2>Executive Board</h2>
       </div>
       <div className="leaders-wrapper">
-      <div className="leaders-grid">
-        {leaders.map((leader, index) => (
-          <div className="leader-box" key={index}>
-            <img src={leader.image} alt={leader.name} className="leader-img" />
-            <h3>{leader.name}</h3>
-            <p>{leader.title}</p>
-          </div>
-        ))}
-      </div>
+        <div className="leaders-grid">
+          {leaders.map((leader, index) => (
+            <div className="leader-box" key={index}>
+              <img src={leader.image} alt={leader.name} className="leader-img" />
+              <h3>{leader.name}</h3>
+              <p>{leader.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
